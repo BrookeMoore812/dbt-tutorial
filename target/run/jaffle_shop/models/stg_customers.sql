294886561eb9bf66bd335298aea580fc3f73e64c
Brooke Moore <brookemoore@pekininsurance.com>
2021-11-11 13:05:14 -0600

    Add customers model, tests, docs

---
+++ target/run/jaffle_shop/models/stg_customers.sql
@@ -0,0 +1,15 @@
+
+
+  create or replace table `dbt-tutorial-331521`.`dbt_brooke`.`stg_customers`
+  
+  
+  OPTIONS()
+  as (
+    SELECT
+id as customer_id
+, first_name
+, last_name
+
+FROM `dbt-tutorial`.jaffle_shop.customers
+  );
+    
---
+++ target/run/jaffle_shop/models/stg_orders.sql
@@ -0,0 +1,16 @@
+
+
+  create or replace table `dbt-tutorial-331521`.`dbt_brooke`.`stg_orders`
+  
+  
+  OPTIONS()
+  as (
+    SELECT
+id as order_id
+, user_id as customer_id
+, order_date
+, STATUS
+
+FROM `dbt-tutorial`.jaffle_shop.orders
+  );
+    
